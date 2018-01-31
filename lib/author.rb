901f6897f7fab2d6984fcacf7dcb00cf437e806a
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-31 14:41:20 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+  attr_accessor :name, :posts
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def self.post_count
+    @@post_count
+  end
+  
+  def add_post(post)
+    @posts << post
+    @@post_count += 1
+    post.author = self
+    post
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    @@post_count += 1
+    post.author = self
+    post
+  end
+  
+end
