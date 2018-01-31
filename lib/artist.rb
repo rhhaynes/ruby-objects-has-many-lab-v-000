3137b7525872188be452dde254eba3589ebff498
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-31 14:29:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,15 @@
 class Artist
   attr_accessor :name, :songs
   
+  @@song_count = 0
+  
   def initialize(name)
     @name = name
     @songs = []
+  end
+  
+  def self.song_count
+    @@song_count
   end
   
   def add_song(song)
