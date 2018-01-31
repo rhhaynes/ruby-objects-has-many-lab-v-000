74c99c5fa951d6078d1ee7a2f12eb2ccfd656d7d
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-31 14:34:32 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,14 @@
 class Author
   attr_accessor :name, :posts
+  @@post_count = 0
   
   def initialize(:name)
     @name = name
     @posts = []
+  end
+  
+  def self.post_count
+    @@post_count
   end
   
   
